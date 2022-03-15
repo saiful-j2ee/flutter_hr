eb8caef49d9f36f90f032f85ebe82d13816ad7da
unknown <Student@NHDL6-S20.nhd.local>
2022-03-15 13:00:17 +0600

    last commit

--- lib/design/employee.dart
+++ lib/design/employee.dart
@@ -1,7 +1,7 @@
-
-
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Employee extends StatefulWidget {
   const Employee({Key? key}) : super(key: key);
@@ -11,37 +11,71 @@ class Employee extends StatefulWidget {
 }
 
 class _EmployeeState extends State<Employee> {
-  String gender="Male";
+  late File imageFile;
+  File? _image;
+
+  String gender = "Male";
+  String date = "";
+  DateTime selectedDate = DateTime.now();
+
+  final _picker = ImagePicker();
+
+  // Implementing the image picker
+  Future<void> _openImagePicker() async {
+    final XFile? pickedImage =
+        await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-
     var _dateOfBirthController;
     return Scaffold(
-      appBar: AppBar(title: Text("Employee Form",textAlign: TextAlign.center,),
-      ),
-        body:Form(
+        appBar: AppBar(
+          title: Text(
+            "Employee Form",
+            textAlign: TextAlign.center,
+          ),
+        ),
+        body: Form(
           child: ListView(
             children: [
+              ElevatedButton(
+                child: const Text('Select An Image'),
+                onPressed: _openImagePicker,
+              ),
+              const SizedBox(height: 35),
+              Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                height: 300,
+                color: Colors.grey[300],
+                child: _image != null
+                    ? Image.file(_image!, fit: BoxFit.cover)
+                    : const Text('Please select an image'),
+              ),
               Padding(
                 padding: const EdgeInsets.all(12.0),
                 child: Center(
                   child: Text(
                     "Employee Form",
                     style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,color:Colors.deepOrange
-                    ),
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepOrange),
                   ),
                 ),
               ),
-
               Padding(
                 padding: const EdgeInsets.only(left: 30.0, right: 30, top: 8),
                 child: TextFormField(
-                 // controller: _usernameController,
+                  // controller: _usernameController,
                   // controller: _usernameController,
                   decoration: InputDecoration(
-
                       suffixIcon: Icon(
                         Icons.supervisor_account_sharp,
                         size: 20.0,
@@ -49,13 +83,13 @@ class _EmployeeState extends State<Employee> {
                       border: OutlineInputBorder(),
                       labelText: "First Name"),
                 ),
-              ),Padding(
+              ),
+              Padding(
                 padding: const EdgeInsets.only(left: 30.0, right: 30, top: 8),
                 child: TextFormField(
-                 // controller: _usernameController,
+                  // controller: _usernameController,
                   // controller: _usernameController,
                   decoration: InputDecoration(
-
                       suffixIcon: Icon(
                         Icons.supervisor_account_sharp,
                         size: 20.0,
@@ -63,13 +97,13 @@ class _EmployeeState extends State<Employee> {
                       border: OutlineInputBorder(),
                       labelText: "Last Name"),
                 ),
-              ),Padding(
+              ),
+              Padding(
                 padding: const EdgeInsets.only(left: 30.0, right: 30, top: 8),
                 child: TextFormField(
-                 // controller: _usernameController,
+                  // controller: _usernameController,
                   // controller: _usernameController,
                   decoration: InputDecoration(
-
                       suffixIcon: Icon(
                         Icons.supervisor_account_sharp,
                         size: 20.0,
@@ -79,200 +113,79 @@ class _EmployeeState extends State<Employee> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 30.0, right: 30, top: 8),
-                child: TextFormField(
-                  controller: _dateOfBirthController,
-                  decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.calendar_today),
-                        onPressed: () => _selectDate(context),
-                      ),
-                      border: UnderlineInputBorder(),
-                      labelText: "Date of Birth"),
-                ),
-              ),
-              Padding(
-
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-
-                    Text("Gender :",textAlign: TextAlign.justify,),
+                    Text(
+                      "Gender :",
+                      textAlign: TextAlign.justify,
+                    ),
                     Row(
                       children: [
-                        Radio(value: "Male", groupValue: gender, onChanged: (val){
-                          gender=val.toString();
-                          print(val);
-                          setState(() {
-
-                          });
-                        }),
+                        Radio(
+                            value: "Male",
+                            groupValue: gender,
+                            onChanged: (val) {
+                              gender = val.toString();
+                              print(val);
+                              setState(() {});
+                            }),
                         Text("Male"),
                       ],
                     ),
                     Row(
                       children: [
-                        Radio(value: "Female", groupValue: gender, onChanged: (val){
-                          gender=val.toString();
-                          print(val);
-                          setState(() {
-
-                          });
-                        }),
+                        Radio(
+                            value: "Female",
+                            groupValue: gender,
+                            onChanged: (val) {
+                              gender = val.toString();
+                              print(val);
+                              setState(() {});
+                            }),
                         Text("Female"),
                       ],
                     ),
                   ],
                 ),
-
               ),
-
-
-
-
-
-
-
-
-
-
-
-              //
-              // Padding(
-              //   padding: const EdgeInsets.only(left: 30.0, right: 30, top: 8),
-              //   child: TextFormField(
-              //    // controller: _usernameController,
-              //     // controller: _usernameController,
-              //     decoration: InputDecoration(
-              //
-              //         suffixIcon: Icon(
-              //           Icons.supervisor_account_sharp,
-              //           size: 20.0,
-              //         ),
-              //         border: OutlineInputBorder(),
-              //         labelText: "Username"),
-              //   ),
-              // ),Padding(
-              //   padding: const EdgeInsets.only(left: 30.0, right: 30, top: 8),
-              //   child: TextFormField(
-              //    // controller: _usernameController,
-              //     // controller: _usernameController,
-              //     decoration: InputDecoration(
-              //
-              //         suffixIcon: Icon(
-              //           Icons.supervisor_account_sharp,
-              //           size: 20.0,
-              //         ),
-              //         border: OutlineInputBorder(),
-              //         labelText: "Username"),
-              //   ),
-              // ),Padding(
-              //   padding: const EdgeInsets.only(left: 30.0, right: 30, top: 8),
-              //   child: TextFormField(
-              //    // controller: _usernameController,
-              //     // controller: _usernameController,
-              //     decoration: InputDecoration(
-              //
-              //         suffixIcon: Icon(
-              //           Icons.supervisor_account_sharp,
-              //           size: 20.0,
-              //         ),
-              //         border: OutlineInputBorder(),
-              //         labelText: "Username"),
-              //   ),
-              // ),Padding(
-              //   padding: const EdgeInsets.only(left: 30.0, right: 30, top: 8),
-              //   child: TextFormField(
-              //    // controller: _usernameController,
-              //     // controller: _usernameController,
-              //     decoration: InputDecoration(
-              //
-              //         suffixIcon: Icon(
-              //           Icons.supervisor_account_sharp,
-              //           size: 20.0,
-              //         ),
-              //         border: OutlineInputBorder(),
-              //         labelText: "Username"),
-              //   ),
-              // ),Padding(
-              //   padding: const EdgeInsets.only(left: 30.0, right: 30, top: 8),
-              //   child: TextFormField(
-              //    // controller: _usernameController,
-              //     // controller: _usernameController,
-              //     decoration: InputDecoration(
-              //
-              //         suffixIcon: Icon(
-              //           Icons.supervisor_account_sharp,
-              //           size: 20.0,
-              //         ),
-              //         border: OutlineInputBorder(),
-              //         labelText: "Username"),
-              //   ),
-              // ),Padding(
-              //   padding: const EdgeInsets.only(left: 30.0, right: 30, top: 8),
-              //   child: TextFormField(
-              //    // controller: _usernameController,
-              //     // controller: _usernameController,
-              //     decoration: InputDecoration(
-              //
-              //         suffixIcon: Icon(
-              //           Icons.supervisor_account_sharp,
-              //           size: 20.0,
-              //         ),
-              //         border: OutlineInputBorder(),
-              //         labelText: "Username"),
-              //   ),
-              // ),
-              // Padding(
-              //   padding: const EdgeInsets.only(left: 30.0, right: 30, top: 8),
-              //   child: TextFormField(
-              //    // controller: _emailController,
-              //     // controller: _emailController,
-              //     decoration: InputDecoration(
-              //         suffixIcon: Icon(
-              //           Icons.mark_email_read,
-              //           size: 20.0,
-              //         ),
-              //         border: OutlineInputBorder(),
-              //         labelText: "Email"),
-              //   ),
-              // ),
-              // Padding(
-              //   padding: const EdgeInsets.only(left: 30.0, right: 30, top: 8),
-              //   child: TextFormField(
-              //   //  controller: _passwordController,
-              //     // controller: _passwordController,
-              //     decoration: InputDecoration(
-              //         suffixIcon: Icon(
-              //           Icons.password,
-              //           size: 20.0,
-              //         ),
-              //         border: OutlineInputBorder(),
-              //         labelText: "Password"),
-              //   ),
-              // ),
-
+              ElevatedButton(
+                onPressed: () {
+                  _selectDate(context);
+                },
+                child: Text("date of birth"),
+              ),
+              Text(
+                  "${selectedDate.day}/${selectedDate.month}/${selectedDate.year}"),
               Padding(
                 padding: const EdgeInsets.all(30.0),
-
                 child: ElevatedButton(
                     onPressed: () {
-
                       //employeeForm();
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => home()),
-                      );
+                      // Navigator.push(
+                      //   context,
+                      //   // MaterialPageRoute(builder: (context) => home()),
+                      // );
                       // save();
                     },
                     child: Text("submit")),
               ),
             ],
           ),
-        )
-    );
+        ));
   }
 
-  home() {}
+  _selectDate(BuildContext context) async {
+    final DateTime? selected = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2010),
+      lastDate: DateTime(2025),
+    );
+    if (selected != null && selected != selectedDate)
+      setState(() {
+        selectedDate = selected;
+      });
+  }
 }
